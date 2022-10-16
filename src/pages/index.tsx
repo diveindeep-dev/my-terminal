9498d3a75c8e324036818e3882a7e19b4ccc902d
hakyung <diveindeep.dev@gmail.com>
2022-10-16 20:11:26 +0900

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next';
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <main>
+        <h1 className="font-bold">My terminal 터미널</h1>
+      </main>
+    </div>
+  );
+};
+
+export default Home;
