25b393d9cc70abe228dde7556bdeedf385fa8358
hakyung <diveindeep.dev@gmail.com>
2022-11-17 20:15:11 +0900

    Add Head for title and favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,23 @@
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>{`myTerminal>_`}</title>
+        <link rel="apple-touch-icon" sizes="180x180" href="static/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="static/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="static/favicon-16x16.png" />
+        <link rel="manifest" href="static/site.webmanifest" />
+        <link rel="mask-icon" href="static/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#b91d47" />
+        <meta name="theme-color" content="#ffffff" />
+      </Head>
+      <Component {...pageProps} />;
+    </>
+  );
 }
 
 export default MyApp;
